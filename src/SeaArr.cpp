a4d3fcb0f34cc4db2b401f44345ff611e8cd0915
Ross Fish <ross@4mscompany.com>
2026-01-12 16:11:59 -0800

    SeaArr: new GUI integrated

--- src/SeaArr.cpp
+++ src/SeaArr.cpp
@@ -3,7 +3,7 @@
 
 struct SeaArr : Module {
 	enum ParamId {
-		SPEED__PARAM,
+		SPEED_PARAM,
 		LENGTH_PARAM,
 		LOOP_PARAM,
 		KICKPUSH_PARAM,
@@ -96,7 +96,7 @@ struct SeaArr : Module {
 	SeaArr() {
 		config(PARAMS_LEN, INPUTS_LEN, OUTPUTS_LEN, LIGHTS_LEN);
 
-		configParam(SPEED__PARAM, 0.f, 1.f, 0.5f, "Speed", "%", 0.f, 100.f);
+		configParam(SPEED_PARAM, 0.f, 1.f, 0.5f, "Speed", "%", 0.f, 100.f);
 		configParam(LENGTH_PARAM, 0.f, 1.f, 1.f, "Length", "%", 0.f, 100.f);
 		configSwitch(LOOP_PARAM, 0.f, 1.f, 0.f, "Loop", {"Off", "On"});
 
@@ -206,7 +206,7 @@ struct SeaArr : Module {
 	}
 
 	void process(const ProcessArgs& args) override {
-		float speed = rescale(params[SPEED__PARAM].getValue(), 0.f, 1.f, SPEED_MIN, SPEED_MAX);
+		float speed = rescale(params[SPEED_PARAM].getValue(), 0.f, 1.f, SPEED_MIN, SPEED_MAX);
 		if (inputs[SPEEDCVIN_INPUT].isConnected()) {
 			speed *= std::clamp(inputs[SPEEDCVIN_INPUT].getVoltage() / 10.f, 0.f, 1.f);
 		}
@@ -236,81 +236,81 @@ struct SeaArr : Module {
 struct SeaArrWidget : ModuleWidget {
 	SeaArrWidget(SeaArr* module) {
 		setModule(module);
-		setPanel(createPanel(asset::plugin(pluginInstance, "res/panels/SeaArr_info.svg")));
+		setPanel(createPanel(asset::plugin(pluginInstance, "res/panels/SeaArr.svg")));
 
-		addChild(createWidget<ScrewBlack>(Vec(RACK_GRID_WIDTH, 0)));
-		addChild(createWidget<ScrewBlack>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, 0)));
-		addChild(createWidget<ScrewBlack>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
-		addChild(createWidget<ScrewBlack>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
+		addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, 0)));
+		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, 0)));
+		addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
+		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 
-		addParam(createParamCentered<Davies1900hBlack>(mm2px(Vec(30.48, 21.308)), module, SeaArr::SPEED__PARAM));
-		addParam(createParamCentered<Davies1900hBlack>(mm2px(Vec(16.002, 33.508)), module, SeaArr::LENGTH_PARAM));
-		addParam(createParamCentered<Switch2Pos>(mm2px(Vec(46.446, 33.508)), module, SeaArr::LOOP_PARAM));
-		
-		addParam(createParamCentered<LEDBezel>(mm2px(Vec(7.0, 59.14)), module, SeaArr::KICKPUSH_PARAM));
-		addChild(createLightCentered<LEDBezelLight<WhiteLight>>(mm2px(Vec(7.0, 59.14)), module, SeaArr::KICK_LIGHT));
+		addParam(createParam<Switch2Pos>(mm2px(Vec(56.4, 12.002)), module, SeaArr::LOOP_PARAM));
+		addParam(createParamCentered<Davies1900hBlack>(mm2px(Vec(16.799, 15.501)), module, SeaArr::LENGTH_PARAM));
+		addParam(createParamCentered<Davies1900hBlack>(mm2px(Vec(38.799, 15.501)), module, SeaArr::SPEED_PARAM));
 
-		addParam(createParamCentered<LEDBezel>(mm2px(Vec(16.49, 59.14)), module, SeaArr::SNAREPUSH_PARAM));
-		addChild(createLightCentered<LEDBezelLight<WhiteLight>>(mm2px(Vec(16.49, 59.14)), module, SeaArr::SNARE_LIGHT));
+		addParam(createParamCentered<LEDBezel>(mm2px(Vec(8.798, 42.002)), module, SeaArr::KICKPUSH_PARAM));
+		addChild(createLightCentered<LEDBezelLight<WhiteLight>>(mm2px(Vec(8.798, 42.002)), module, SeaArr::KICK_LIGHT));
 		
-		addParam(createParamCentered<LEDBezel>(mm2px(Vec(25.4, 59.14)), module, SeaArr::HATPUSH_PARAM));
-		addChild(createLightCentered<LEDBezelLight<WhiteLight>>(mm2px(Vec(25.4, 59.14)), module, SeaArr::HAT_LIGHT));
+		addParam(createParamCentered<LEDBezel>(mm2px(Vec(20.5, 42.002)), module, SeaArr::SNAREPUSH_PARAM));
+		addChild(createLightCentered<LEDBezelLight<WhiteLight>>(mm2px(Vec(20.5, 42.002)), module, SeaArr::SNARE_LIGHT));
 		
-		addParam(createParamCentered<LEDBezel>(mm2px(Vec(35.318, 59.14)), module, SeaArr::HATMETALPUSH_PARAM));
-		addChild(createLightCentered<LEDBezelLight<WhiteLight>>(mm2px(Vec(35.318, 59.14)), module, SeaArr::HATMETAL_LIGHT));
+		addParam(createParamCentered<LEDBezel>(mm2px(Vec(32.3, 42.002)), module, SeaArr::HATPUSH_PARAM));
+		addChild(createLightCentered<LEDBezelLight<WhiteLight>>(mm2px(Vec(32.3, 42.002)), module, SeaArr::HAT_LIGHT));
+		
+		addParam(createParamCentered<LEDBezel>(mm2px(Vec(43.998, 42.002)), module, SeaArr::HATMETALPUSH_PARAM));
+		addChild(createLightCentered<LEDBezelLight<WhiteLight>>(mm2px(Vec(43.998, 42.002)), module, SeaArr::HATMETAL_LIGHT));
+		
+		addParam(createParamCentered<LEDBezel>(mm2px(Vec(55.7, 42.002)), module, SeaArr::RIMPUSH_PARAM));
+		addChild(createLightCentered<LEDBezelLight<WhiteLight>>(mm2px(Vec(55.7, 42.002)), module, SeaArr::RIM_LIGHT));
+		
+		addParam(createParamCentered<LEDBezel>(mm2px(Vec(67.501, 42.002)), module, SeaArr::COWPUSH_PARAM));
+		addChild(createLightCentered<LEDBezelLight<WhiteLight>>(mm2px(Vec(67.501, 42.002)), module, SeaArr::COW_LIGHT));
+		
+		addParam(createParamCentered<LEDBezel>(mm2px(Vec(8.798, 84.999)), module, SeaArr::CONGAPUSH_PARAM));
+		addChild(createLightCentered<LEDBezelLight<WhiteLight>>(mm2px(Vec(8.798, 84.999)), module, SeaArr::CONGA_LIGHT));
+		
+		addParam(createParamCentered<LEDBezel>(mm2px(Vec(20.5, 84.999)), module, SeaArr::BONGOLPUSH_PARAM));
+		addChild(createLightCentered<LEDBezelLight<WhiteLight>>(mm2px(Vec(20.5, 84.999)), module, SeaArr::BONGOL_LIGHT));
+		
+		addParam(createParamCentered<LEDBezel>(mm2px(Vec(32.3, 84.999)), module, SeaArr::BONGOHPUSH_PARAM));
+		addChild(createLightCentered<LEDBezelLight<WhiteLight>>(mm2px(Vec(32.3, 84.999)), module, SeaArr::BONGOH_LIGHT));
+		
+		addParam(createParamCentered<LEDBezel>(mm2px(Vec(43.998, 84.999)), module, SeaArr::TAMPUSH_PARAM));
+		addChild(createLightCentered<LEDBezelLight<WhiteLight>>(mm2px(Vec(43.998, 84.999)), module, SeaArr::TAM_LIGHT));
+		
+		addParam(createParamCentered<LEDBezel>(mm2px(Vec(55.7, 84.999)), module, SeaArr::GUIROPUSH_PARAM));
+		addChild(createLightCentered<LEDBezelLight<WhiteLight>>(mm2px(Vec(55.7, 84.999)), module, SeaArr::GUIRO_LIGHT));
+		
+		addParam(createParamCentered<LEDBezel>(mm2px(Vec(67.501, 84.999)), module, SeaArr::CYMPUSH_PARAM));
+		addChild(createLightCentered<LEDBezelLight<WhiteLight>>(mm2px(Vec(67.501, 84.999)), module, SeaArr::CYM_LIGHT));		
 
-		addParam(createParamCentered<LEDBezel>(mm2px(Vec(44.699, 59.14)), module, SeaArr::RIMPUSH_PARAM));
-		addChild(createLightCentered<LEDBezelLight<WhiteLight>>(mm2px(Vec(44.699, 59.14)), module, SeaArr::RIM_LIGHT));
-		
-		addParam(createParamCentered<LEDBezel>(mm2px(Vec(54.519, 59.14)), module, SeaArr::COWPUSH_PARAM));
-		addChild(createLightCentered<LEDBezelLight<WhiteLight>>(mm2px(Vec(54.519, 59.14)), module, SeaArr::COW_LIGHT));
-		
-		addParam(createParamCentered<LEDBezel>(mm2px(Vec(7.0, 94.743)), module, SeaArr::CONGAPUSH_PARAM));
-		addChild(createLightCentered<LEDBezelLight<WhiteLight>>(mm2px(Vec(7.0, 94.743)), module, SeaArr::CONGA_LIGHT));
-		
-		addParam(createParamCentered<LEDBezel>(mm2px(Vec(16.49, 94.743)), module, SeaArr::BONGOLPUSH_PARAM));
-		addChild(createLightCentered<LEDBezelLight<WhiteLight>>(mm2px(Vec(16.49, 94.743)), module, SeaArr::BONGOL_LIGHT));
-		
-		addParam(createParamCentered<LEDBezel>(mm2px(Vec(25.4, 94.743)), module, SeaArr::BONGOHPUSH_PARAM));
-		addChild(createLightCentered<LEDBezelLight<WhiteLight>>(mm2px(Vec(25.4, 94.743)), module, SeaArr::BONGOH_LIGHT));
-		
-		addParam(createParamCentered<LEDBezel>(mm2px(Vec(35.318, 94.743)), module, SeaArr::TAMPUSH_PARAM));
-		addChild(createLightCentered<LEDBezelLight<WhiteLight>>(mm2px(Vec(35.318, 94.743)), module, SeaArr::TAM_LIGHT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(16.849, 30.801)), module, SeaArr::LENGTHCVIN_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(38.851, 30.801)), module, SeaArr::SPEEDCVIN_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(59.351, 30.801)), module, SeaArr::LOOPCVIN_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(8.798, 56.0)), module, SeaArr::KICKTRIGIN_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(20.5, 56.0)), module, SeaArr::SNARETRIGIN_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(32.3, 56.0)), module, SeaArr::HATTRIGIN_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(43.998, 56.0)), module, SeaArr::HATMETALTRIGIN_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(55.7, 56.0)), module, SeaArr::RIMTRIGIN_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(67.501, 56.0)), module, SeaArr::COWTRIGIN_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(8.798, 99.001)), module, SeaArr::CONGATRIGIN_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(20.5, 99.001)), module, SeaArr::BONGOLTRIGIN_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(32.3, 99.001)), module, SeaArr::BONGOHTRIGIN_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(43.998, 99.001)), module, SeaArr::TAMTRIGIN_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(55.7, 99.001)), module, SeaArr::GUIROTRIGIN_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(67.501, 99.001)), module, SeaArr::CYMTRIGIN_INPUT));
 
-		addParam(createParamCentered<LEDBezel>(mm2px(Vec(44.699, 94.743)), module, SeaArr::GUIROPUSH_PARAM));
-		addChild(createLightCentered<LEDBezelLight<WhiteLight>>(mm2px(Vec(44.699, 94.743)), module, SeaArr::GUIRO_LIGHT));
-		
-		addParam(createParamCentered<LEDBezel>(mm2px(Vec(54.519, 94.743)), module, SeaArr::CYMPUSH_PARAM));
-		addChild(createLightCentered<LEDBezelLight<WhiteLight>>(mm2px(Vec(54.519, 94.743)), module, SeaArr::CYM_LIGHT));
-
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(30.48, 33.97)), module, SeaArr::SPEEDCVIN_INPUT));
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(16.002, 45.349)), module, SeaArr::LENGTHCVIN_INPUT));
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(46.446, 45.349)), module, SeaArr::LOOPCVIN_INPUT));
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(7.0, 71.357)), module, SeaArr::KICKTRIGIN_INPUT));
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(16.49, 71.357)), module, SeaArr::SNARETRIGIN_INPUT));
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(25.4, 71.357)), module, SeaArr::HATTRIGIN_INPUT));
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(35.318, 71.357)), module, SeaArr::HATMETALTRIGIN_INPUT));
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(44.699, 71.357)), module, SeaArr::RIMTRIGIN_INPUT));
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(54.519, 71.357)), module, SeaArr::COWTRIGIN_INPUT));
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(7.0, 106.959)), module, SeaArr::CONGATRIGIN_INPUT));
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(16.49, 106.959)), module, SeaArr::BONGOLTRIGIN_INPUT));
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(25.4, 106.959)), module, SeaArr::BONGOHTRIGIN_INPUT));
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(35.318, 106.959)), module, SeaArr::TAMTRIGIN_INPUT));
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(44.699, 106.959)), module, SeaArr::GUIROTRIGIN_INPUT));
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(54.519, 106.959)), module, SeaArr::CYMTRIGIN_INPUT));
-
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(7.0, 83.813)), module, SeaArr::KICKOUT_OUTPUT));
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(16.49, 83.813)), module, SeaArr::SNAREOUT_OUTPUT));
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(25.4, 83.813)), module, SeaArr::HATOUT_OUTPUT));
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(35.318, 83.813)), module, SeaArr::HATMETALOUT_OUTPUT));
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(44.699, 83.813)), module, SeaArr::RIMOUT_OUTPUT));
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(54.519, 83.813)), module, SeaArr::COWOUT_OUTPUT));
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(7.0, 118.357)), module, SeaArr::CONGAOUT_OUTPUT));
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(16.49, 118.357)), module, SeaArr::BONGOLOUT_OUTPUT));
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(25.4, 118.357)), module, SeaArr::BONGOHOUT_OUTPUT));
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(35.318, 118.357)), module, SeaArr::TAMOUT_OUTPUT));
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(44.699, 118.357)), module, SeaArr::GUIROOUT_OUTPUT));
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(54.519, 118.357)), module, SeaArr::CYMOUT_OUTPUT));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(8.798, 70.002)), module, SeaArr::KICKOUT_OUTPUT));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(20.5, 70.002)), module, SeaArr::SNAREOUT_OUTPUT));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(32.3, 70.002)), module, SeaArr::HATOUT_OUTPUT));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(43.998, 70.002)), module, SeaArr::HATMETALOUT_OUTPUT));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(55.7, 70.002)), module, SeaArr::RIMOUT_OUTPUT));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(67.501, 70.002)), module, SeaArr::COWOUT_OUTPUT));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(8.798, 112.999)), module, SeaArr::CONGAOUT_OUTPUT));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(20.5, 112.999)), module, SeaArr::BONGOLOUT_OUTPUT));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(32.3, 112.999)), module, SeaArr::BONGOHOUT_OUTPUT));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(43.998, 112.999)), module, SeaArr::TAMOUT_OUTPUT));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(55.7, 112.999)), module, SeaArr::GUIROOUT_OUTPUT));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(67.501, 112.999)), module, SeaArr::CYMOUT_OUTPUT));
 	}
 };
 
