ab549e3c921127622d8bf22496faa0494b2a16a6
Ross Fish <ross@4mscompany.com>
2026-02-23 11:57:16 -0800

    SicksOh: replaced samples with royalty free samples from Splice. Changed playback speed max to 2x

--- src/SicksOh.cpp
+++ src/SicksOh.cpp
@@ -82,7 +82,7 @@ struct SicksOh : Module {
 	SicksOh() {
 		config(PARAMS_LEN, INPUTS_LEN, OUTPUTS_LEN, LIGHTS_LEN);
 
-		configParam(SPEED_PARAM, 0.f, 1.f, 0.5f, "Speed", "%", 0.f, 100.f);
+		configParam(SPEED_PARAM, 0.f, 2.f, 1.f, "Speed", "%", 0.f, 100.f);
 		configParam(LENGTH_PARAM, 0.f, 1.f, 1.f, "Length", "%", 0.f, 100.f);
 		configSwitch(LOOP_PARAM, 0.f, 1.f, 0.f, "Loop", {"Off", "On"});
 
@@ -157,7 +157,7 @@ struct SicksOh : Module {
 		// Precompute speed (scaled with CV), std::clamp and map to range
 		float knobSpeed = 0.01f + params[SPEED_PARAM].getValue() * (1.0f - 0.01f);
 		float speedCV = std::clamp(inputs[SPEEDCVIN_INPUT].getVoltage(), -5.f, 5.f);
-		float normSpeed = std::clamp(knobSpeed + (speedCV / 5.f) * 0.5f, 0.01f, 1.0f);
+		float normSpeed = std::clamp(knobSpeed + (speedCV / 5.f) * 0.5f, 0.01f, 2.0f);
 		float speed = SPEED_LOW + (normSpeed - 0.01f) * ((SPEED_HIGH - SPEED_LOW) / (1.0f - 0.01f));
 	
 		// Precompute length ratio similarly
